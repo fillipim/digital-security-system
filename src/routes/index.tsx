2918c8e94bf2b90f3f58872f2ef213c1076b641a
Filipe Monteiro Fernandes <filipemonteirofernandes9@gmail.com>
2023-01-18 12:05:54 -0300

    feat: add initial configs

---
+++ src/routes/index.tsx
@@ -0,0 +1,5 @@
+import { Route, Routes } from "react-router-dom";
+
+const MainRoutes = () => <Routes></Routes>;
+
+export default MainRoutes;
---
+++ src/styles/theme/colors/index.ts
@@ -0,0 +1,19 @@
+const colors: any = {
+    brand: {
+        100: "##00ffaa",
+        200: "##00b679"
+    },
+    grey: {
+        grey0: "#F8F9FA",
+        grey1: "#868E96",
+        grey2: "#343B41",
+        grey3: "#212529",
+        grey4: "#121214"
+    },
+    feedeback: {
+        error: "#E83F5B",
+        sucess: "#3FE864"
+    }
+};
+
+export default colors;
